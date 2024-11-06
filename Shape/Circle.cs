0fbd96c9dca44adaf6b4fccb4f524fe1857f0dde
Nikita <kalyazin.nik@yandex.ru>
2024-11-06 20:36:21 +0300

    Создал класс Circle, реализовал интерфейс IShape. Так же добавил методы вычисления площади сектора по углу в градусах и радианах.

---
+++ Shape/Circle.cs
@@ -0,0 +1,21 @@
+﻿namespace Shape;
+
+public class Circle(double radius) : IShape
+{
+    private double _area = Math.PI * radius * radius;
+    private double _perimeter = Math.PI * 2 * radius;
+
+    public double Radius { get; } = radius;
+    public double Area => _area;
+    public double Perimeter => _perimeter;
+
+    public double GetSectorArea(double radiansAngle)
+    {
+        return Radius * Radius * (radiansAngle / 2);
+    }
+    
+    public double GetSectorArea(float degreesAngle)
+    {
+        return _area * degreesAngle / 360;
+    }
+}
