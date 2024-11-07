edce6654049126b8ff4cd34b5688396c47ee4b40
Nikita <kalyazin.nik@yandex.ru>
2024-11-07 10:48:07 +0300

    Исправил класс Circle согласно прохождению тестов: добавил метод выбрасывания исключения для радиуса равного или меньшего нуля; расчет площади секторов по градусам и радианам. В тестах снизил погрешность в вычислениях до 5 знаков после запятой.

--- Shape/Circle.cs
+++ Shape/Circle.cs
@@ -1,28 +1,45 @@
 ﻿namespace Shape;
 
-public class Circle(double radius) : IShape
+public class Circle : IShape
 {
-    public double Radius { get; } = radius;
-    public double Area { get; } = CalculateArea(radius);
-    public double Circumference { get; } = GetCircumference(radius);
+    public double Radius { get; }
+    public double Area { get; }
+    public double Circumference { get; }
+
+    public Circle(double radius)
+    {
+        ThrowIfRadiusIsLessThanOrEqualZero(radius);
+        Radius = radius;
+        Area = CalculateArea(radius);
+        Circumference = GetCircumference(radius);
+    }
+
+    private static void ThrowIfRadiusIsLessThanOrEqualZero(double radius)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(radius, 0);
+    }
 
     public double CalculateArea()
         => CalculateArea(Radius);
 
     public double GetSectorArea(double radiansAngle)
         => GetSectorArea(Radius, radiansAngle);
-    
+
     public double GetSectorArea(float degreesAngle)
         => GetSectorArea(Radius, degreesAngle);
 
     public static double GetSectorArea(double radius, double radiansAngle)
     {
+        radiansAngle = Math.Abs(radiansAngle);
+        radiansAngle = radiansAngle - Math.Tau * Math.Floor(radiansAngle / Math.Tau);
         return radius * radius * (radiansAngle / 2);
     }
-    
+
     public static double GetSectorArea(double radius, float degreesAngle)
     {
-        return GetCircumference(radius) * degreesAngle / 360;
+        degreesAngle = Math.Abs(degreesAngle);
+        degreesAngle = (float)(degreesAngle - 360 * Math.Floor(degreesAngle / 360));
+        return CalculateArea(radius) * (degreesAngle / 360);
     }
 
     public static double CalculateArea(double radius)
